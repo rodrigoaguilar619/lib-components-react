caa6f78d7c939cc727a9e5e789b5b5d71e1e97ab
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2024-05-30 22:34:48 -0600

    Added size to hook for modal data

--- src/@types/components/_hooks/modalHook.d.ts
+++ src/@types/components/_hooks/modalHook.d.ts
@@ -2,4 +2,5 @@ export interface ModalState {
     showModal: boolean;
     bodyModal: ReactNode | undefined;
     titleModal: string;
+    size: "sm" | "md" | "lg";
 }
--- src/hookStates/modalHookState.tsx
+++ src/hookStates/modalHookState.tsx
@@ -1,11 +1,12 @@
 import { ModalState } from '@app/@types/components/_hooks/modalHook';
 import { useState, ReactNode } from 'react';
 
-const useHookModal = (): [ModalState, () => void, () => void, (body: ReactNode) => void, (title: string) => void] => {
+const useHookModal = (): [ModalState, () => void, () => void, (body: ReactNode) => void, (title: string) => void, (size: "sm" | "md" | "lg") => void] => {
     const [modalState, setModalState] = useState<ModalState>({
         showModal: false,
         bodyModal: undefined,
         titleModal: '',
+        size: "md",
     });
 
     const setOpenModal = () => {
@@ -36,7 +37,14 @@ const useHookModal = (): [ModalState, () => void, () => void, (body: ReactNode) 
         }));
     };
 
-    return [modalState, setOpenModal, setCloseModal, setBodyModal, setTitleModal];
+    const setSizeModal = (size: "sm" | "md" | "lg") => {
+        setModalState(prevState => ({
+            ...prevState,
+            size: size,
+        }));
+    };
+
+    return [modalState, setOpenModal, setCloseModal, setBodyModal, setTitleModal, setSizeModal];
 };
 
 export default useHookModal;
