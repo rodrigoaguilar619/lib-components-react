876e02cf39c9b413ae951aff7d397a3152922656
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2024-05-30 22:35:17 -0600

    Adjust code to optimize for modal component

--- src/components/modals/modalComponent.tsx
+++ src/components/modals/modalComponent.tsx
@@ -1,38 +1,50 @@
+import React from "react";
 import { ModalPropsI } from "@app/@types/components/modals/modal";
 import { ComponentTypeEnum } from "@app/catalogs/enumCatalog";
 import AlertLayout from "@app/templates/environments/coreui/template/layouts/alertLayout";
 import { Dialog } from "primereact/dialog";
-import { useEffect } from "react";
+
+const getModalStyles = (size: string | undefined) => {
+    let styles = {};
+    let maximizable = false;
+
+    switch (size) {
+        case "sm":
+            styles = { top: "50px", width: '70%', minHeight: '10%' };
+            break;
+        case "md":
+            styles = { top: "50px", width: '80%', minHeight: '30%' };
+            break;
+        case "lg":
+            styles = { width: '95%', minHeight: '90%' };
+            maximizable = true;
+            break;
+        default:
+            styles = { top: "50px", width: '80%', minHeight: '10%' };
+    }
+
+    return { styles, maximizable };
+};
 
 const ModalComponent: React.FC<ModalPropsI> = (props) => {
 
-    useEffect(() => {
+    const { styles, maximizable } = getModalStyles(props.size);
+    const position: "center" | "top" | undefined = "top";
 
-        return () => {
-        };
-    }, []);
+    if (!props.visible) return null;
 
-    let styles = {};
-    let maximizable = false;
-    let position: "center" | "top" | undefined = "top";
-
-    if (props.size === "sm") {
-        styles = { top: "50px", width: '70%', minHeight: '10%' };
-    } else if (props.size === "md") {
-        styles = { top: "50px", width: '80%', minHeight: '30%' };
-    } else if (props.size === "lg") {
-        styles = { width: '95%', minHeight: '90%' };
-        maximizable = true;
-    }
-    else {
-        styles = { top: "50px", width: '80%', minHeight: '10%' };
-    }
-
-    return (<Dialog header={props.title} visible={props.visible} maximizable={maximizable}
-        position={position} style={styles} onHide={() => props.selectorCloseModal()}>
-        <AlertLayout componentType={ComponentTypeEnum.POPUP} />
-        {props.body}
-    </Dialog>
+    return (
+        <Dialog
+            header={props.title}
+            visible={props.visible}
+            maximizable={maximizable}
+            position={position}
+            style={styles}
+            onHide={() => props.selectorCloseModal()}
+        >
+            <AlertLayout componentType={ComponentTypeEnum.POPUP} />
+            {props.body}
+        </Dialog>
     );
 }
 
