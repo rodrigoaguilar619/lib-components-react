3f2789d74b81cc0766df48fab0e5b198258418ff
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2024-05-30 22:35:49 -0600

    Development of loading module component

---
+++ src/components/loadings/loadingModuleComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoadingModuleComponent: React.FC<any> = (props: any) => {
+
+        return (
+            <div>Loading...</div>
+        )
+}
+
+export default LoadingModuleComponent
