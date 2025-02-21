c65f62e94c40d190a214f8cda682672b8f6261b9
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2025-02-21 00:06:06 -0600

    Implemented version project on header dropdown template primereact

--- src/templates/environments/primeReact/template/layouts/headerDropdown.tsx
+++ src/templates/environments/primeReact/template/layouts/headerDropdown.tsx
@@ -4,12 +4,14 @@ import { useSelector } from 'react-redux'
 import { TemplateUserStateI } from '@app/@types/controller/reducers/templateUserReducer'
 import { formatToCapitalize } from '@app/utils/formatUtils/formatStringUtil'
 import { useEffect, useRef, useState } from 'react'
+import { _APP_VERSION_ } from '@app/catalogs/constantCatalog'
 
 const HeaderDropdown = () => {
 
   const templateUserDataState: TemplateUserStateI = useSelector((state: any) => state.templateUserDataState);
   const [overLayVisible, setOverLayVisible] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const appVersion = _APP_VERSION_;
 
   useEffect(() => {
     function handleClickOutside(event: any) {
@@ -30,7 +32,9 @@ const HeaderDropdown = () => {
   const renderOverlay = () => {
     return (<div className="dropdown">
       <div className="dropdown-content">
-        <div className="dropdown-header dropdown-header-first">User:</div>
+        <div className="dropdown-header dropdown-header-first">Version:</div>
+        <a className="dropdown-item">{appVersion}</a>
+        <div className="dropdown-header dropdown-header">User:</div>
         <a className="dropdown-item">{templateUserDataState.userName}</a>
         <div className="dropdown-header">Roles</div>
         {buildRoles(templateUserDataState.userRols)}
