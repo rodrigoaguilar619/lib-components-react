209774241a474d67de0c3b32b10ed10c3248d5d1
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2025-02-21 00:05:50 -0600

    Implemented version project on header dropdown template coreui

--- src/templates/environments/coreui/template/layouts/headerDropdown.tsx
+++ src/templates/environments/coreui/template/layouts/headerDropdown.tsx
@@ -10,11 +10,13 @@ import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { useSelector } from 'react-redux'
 import { TemplateUserStateI } from '@app/@types/controller/reducers/templateUserReducer'
 import { formatToCapitalize } from '@app/utils/formatUtils/formatStringUtil'
+import { _APP_VERSION_ } from '@app/catalogs/constantCatalog'
 
 const HeaderDropdown = () => {
 
   const templateUserDataState: TemplateUserStateI = useSelector((state: any) => state.templateUserDataState);
   const itemStyle = { backgroundColor: "transparent", outline: "none", color: "inherit" }
+  const appVersion = _APP_VERSION_;
 
   const buildRoles = (roles: string[]) => {
     return roles.map((rol: string) => { return <div>{formatToCapitalize(rol)}</div> })
@@ -28,6 +30,10 @@ const HeaderDropdown = () => {
         <div style={{ float: "right", scale: "0.7" }}>▼</div>
       </CDropdownToggle>
       <CDropdownMenu className="pt-0">
+        <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Version:</CDropdownHeader>
+        <CDropdownItem className='dropdown-item-first' style={{...itemStyle }}>
+          {appVersion}
+        </CDropdownItem>
         <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">User:</CDropdownHeader>
         <CDropdownItem style={itemStyle}>
           {templateUserDataState.userName}
